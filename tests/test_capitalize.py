29c710033e718445d50d917a911f17ff73c82dff
maksimov.yd <maksimov.yd@gmail.com>
2023-08-16 14:43:40 +0300

    IF -> ASSER new type tests

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -8,10 +8,7 @@
 
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception('Функция работает неверно!')
-
-if capitalize('') != '':
-    raise Exception('Функция работает неверно!')
+assert capitalize('') == ''
+assert capitalize('hello') == 'Hello'
 
 print('Все тесты пройдены!')
